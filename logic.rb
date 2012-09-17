56a9309e9e0b208f2cd428221f55b3e211e49da7
Will Madden <w.a.madden@gmail.com>
2012-09-17 18:35:05 +1000

    Fixed sample logic.

--- logic.rb
+++ logic.rb
@@ -4,5 +4,5 @@ end
 
 def choose_tile_to_slide(current_board, solved_board)
   # Return the tile to slide (identified by its position, as a Point)
-  current_board.points_adjacent_to_gap.sample
+  current_board.movable_tiles.sample
 end
